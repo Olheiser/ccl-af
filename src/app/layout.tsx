d0e4dff8ae03acb4399e62383066522bd20de02d
Olheiser <tanner.olheiser@gmail.com>
2025-02-11 16:19:12 -0600

    Added meta tag to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#bb0000" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
